9dda22ef2f410e7112811aa59df049f819a84860
ARProxy <hocci0222@kakao.com>
2025-11-01 19:09:03 +0900

    👔 Feat: 로또 구입 금액을 입력 받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입 금액을 입력해 주세요.");
+        //유효성 검사 : 천원단위인가?
+        int pay = Integer.parseInt(Console.readLine());
+
+
+
     }
 }
